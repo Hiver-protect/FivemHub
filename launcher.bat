a0ca3b4fe86f501823bcf58b367bf8641fbcec0e
Hiver-protect <jdp8532@gmail.com>
2026-08-27 19:54:45 -0400

    feat: Enforce true native full-screen maximized mode

--- launcher.bat
+++ launcher.bat
@@ -27,18 +27,18 @@ if exist "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe" (
 :: 3. INSTALLE LE RACCOURCI APPLICATION SUR LE BUREAU WINDOWS
 set "SCRIPT_JS=%temp%\create_fivem_shortcut.js"
 if defined APP_EXE (
-    echo var wsh = new ActiveXObject("WScript.Shell"^); var sc = wsh.CreateShortcut(wsh.SpecialFolders("Desktop"^)^ + "\\FiveM Hub Launcher.lnk"^); sc.TargetPath = "%APP_EXE%"; sc.Arguments = "--app=http://localhost:3000 --window-size=1500,920 --autoplay-policy=no-user-gesture-required"; sc.WorkingDirectory = "%~dp0"; sc.IconLocation = "%~dp0logo.ico, 0"; sc.Description = "FiveM Hub Launcher - Application Officielle"; sc.Save(^); > "%SCRIPT_JS%"
+    echo var wsh = new ActiveXObject("WScript.Shell"^); var sc = wsh.CreateShortcut(wsh.SpecialFolders("Desktop"^)^ + "\\FiveM Hub Launcher.lnk"^); sc.TargetPath = "%APP_EXE%"; sc.Arguments = "--app=http://localhost:3000 --start-maximized --autoplay-policy=no-user-gesture-required"; sc.WorkingDirectory = "%~dp0"; sc.IconLocation = "%~dp0logo.ico, 0"; sc.Description = "FiveM Hub Launcher - Application Officielle"; sc.Save(^); > "%SCRIPT_JS%"
 ) else (
     echo var wsh = new ActiveXObject("WScript.Shell"^); var sc = wsh.CreateShortcut(wsh.SpecialFolders("Desktop"^)^ + "\\FiveM Hub Launcher.lnk"^); sc.TargetPath = "%~dp0launcher.bat"; sc.WorkingDirectory = "%~dp0"; sc.IconLocation = "%~dp0logo.ico, 0"; sc.Description = "FiveM Hub Launcher"; sc.WindowStyle = 7; sc.Save(^); > "%SCRIPT_JS%"
 )
 cscript //nologo "%SCRIPT_JS%" >nul 2>&1
 del "%SCRIPT_JS%" >nul 2>&1
 
-:: 4. OUVRE IMMEDIATEMENT L'APPLICATION EN FENETRE LOGICIELLE AUTONOME
+:: 4. OUVRE IMMEDIATEMENT L'APPLICATION EN PLEIN ECRAN TOTAL MAXIMISE
 rd /s /q "%temp%\FiveMHubApp" >nul 2>&1
 if defined APP_EXE (
-    start "" "%APP_EXE%" --app="http://localhost:3000" --window-size=1500,920 --user-data-dir="%temp%\FiveMHubApp" --autoplay-policy=no-user-gesture-required --disk-cache-size=1
+    start "" "%APP_EXE%" --app="http://localhost:3000" --start-maximized --user-data-dir="%temp%\FiveMHubApp" --autoplay-policy=no-user-gesture-required --disk-cache-size=1
 ) else (
-    start "" msedge --app="http://localhost:3000" --window-size=1500,920 --user-data-dir="%temp%\FiveMHubApp" --autoplay-policy=no-user-gesture-required --disk-cache-size=1
+    start "" msedge --app="http://localhost:3000" --start-maximized --user-data-dir="%temp%\FiveMHubApp" --autoplay-policy=no-user-gesture-required --disk-cache-size=1
 )
 exit
